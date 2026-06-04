1ae5e9407d7ebd512f2d9116f561810f5e5a99dc
Gautam_kushwaha <gautamkushwha4467@gmail.com>
2026-06-04 18:36:50 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,31 @@
+const trades = [
+  {
+    id: 1,
+    date: "2026-06-01",
+    symbol: "XAUUSD",
+    type: "Long",
+    result: "win",
+    pnl: 400,
+    rr: 2.1
+  },
+  {
+    id: 2,
+    date: "2026-06-02",
+    symbol: "NAS100",
+    type: "Short",
+    result: "loss",
+    pnl: -150,
+    rr: -1
+  },
+  {
+    id: 3,
+    date: "2026-06-03",
+    symbol: "EURUSD",
+    type: "Long",
+    result: "win",
+    pnl: 350,
+    rr: 2.4
+  }
+];
+
+window.trades = trades;
